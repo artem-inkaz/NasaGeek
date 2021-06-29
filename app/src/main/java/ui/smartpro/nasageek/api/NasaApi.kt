f1ead922bc30e1a97980d1c4f702e22a6ffdd786
artemternovenko <artem.inkaz@gmail.com>
2021-06-29 17:29:56 +0600

    [ADDED] api package

--- app/src/main/java/ui/smartpro/nasageek/api/NasaApi.kt
+++ app/src/main/java/ui/smartpro/nasageek/api/NasaApi.kt
@@ -1,4 +1,10 @@
 package ui.smartpro.nasageek.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NasaApi {
+
+    @GET("planetary/apod")
+   suspend fun getPictureOfTheDay(@Query("api_key") apiKey: String): NasaModel
 }
