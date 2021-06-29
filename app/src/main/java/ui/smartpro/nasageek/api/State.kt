f8df266edcec80eae76dd542badc65b6a79fd7e2
artemternovenko <artem.inkaz@gmail.com>
2021-06-29 23:09:03 +0600

    [ADDED] drawable, ViewModel

--- app/src/main/java/ui/smartpro/nasageek/api/State.kt
+++ app/src/main/java/ui/smartpro/nasageek/api/State.kt
@@ -1,6 +1,7 @@
 package ui.smartpro.nasageek.api
 
 sealed class State{
+    class Init : State()
     data class Success(val serverResponseData: NasaModel) : State()
     data class Error(val error: Throwable) : State()
     data class Loading(val progress: Int?) : State()
